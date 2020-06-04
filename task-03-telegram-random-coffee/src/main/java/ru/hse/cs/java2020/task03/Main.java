4de99c588fb5a9256fb0ca24b095c5a59e16bb24
s17b2-voroneckij <34840323+s17b2-voroneckij@users.noreply.github.com>
2020-06-04 23:07:53 +0300

    Update Main.java

--- task-03-telegram-random-coffee/src/main/java/ru/hse/cs/java2020/task03/Main.java
+++ task-03-telegram-random-coffee/src/main/java/ru/hse/cs/java2020/task03/Main.java
@@ -1,7 +1,13 @@
 package ru.hse.cs.java2020.task03;
 
+import com.pengrad.telegrambot.TelegramBot;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        var db = new UsersDatabase("/home/dima/test.db");
+        var trackerClient = TrackerClient.getTrackerClient();
+        var bot = new TelegramBot("854909600:AAHB77kIuoSnks7C57UVkpeU8VJIhNEeKmg");
+        var trackerBot = new TrackerBot(bot, db, trackerClient);
+        trackerBot.run();
     }
 }
